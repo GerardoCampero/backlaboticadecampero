dc9f4099383139cfac9c2043c42a8d1b8f0e692f
Gerardo <geragrungeeb@gmail.com>
2025-03-17 15:05:51 -0300

    Se suben ultimas modificaciones

--- config/database.py
+++ config/database.py
@@ -1,11 +1,33 @@
+import os
 from sqlmodel import SQLModel, create_engine
+from dotenv import load_dotenv
 
-DATABASE_URL = 'sqlite:///config/la_botica_db.db'
+# Cargar las variables de entorno desde el archivo .env
+load_dotenv()
 
+# Verificar que las variables de entorno están siendo cargadas
+print("Cargando variables de entorno...")
+
+# Corregir el nombre de la variable de entorno a 'environment'
+environment = os.getenv('ENVIRONMENT', 'dev')
+print(f"Entorno seleccionado: {environment}")
+
+# Definir la URL de la base de datos dependiendo del entorno
+if environment == 'prd':
+    DATABASE_URL = os.getenv("DATABASE_URL_PRD")
+else:
+    DATABASE_URL = os.getenv("DATABASE_URL_DEV")
+
+# Imprimir la URL de la base de datos
+print(f"DATABASE_URL: {DATABASE_URL}")
+
+# Crear el motor de la base de datos
 engine = create_engine(DATABASE_URL)
 
+# Función para iniciar la base de datos (crear tablas)
 def iniciar_db():
-    SQLModel.metadata.create_all(engine) 
+    SQLModel.metadata.create_all(engine)
 
+# Función para borrar la base de datos (eliminar tablas)
 def borrar_db():
     SQLModel.metadata.drop_all(engine)
